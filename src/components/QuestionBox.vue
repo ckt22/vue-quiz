0960c8c6b4e046fd77419f6c8e9f744a1331961c
CK TANG <cktangad@connect.ust.hk>
2020-04-13 21:42:05 +0800

    First Commit

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="question-box-container">
+  <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">
+
+    <template v-slot:lead>
+        Some question here?
+    </template>
+
+    <hr class="my-4">
+
+    <p>
+        List of answers
+    </p>
+
+    <b-button variant="primary" href="#">Submit</b-button>
+    <b-button variant="success" href="#">Next</b-button>
+  </b-jumbotron>
+</div>
+</template>
